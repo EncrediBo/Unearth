69f4e0fb71c244996d266b7660eb6ec31e651d80
EncrediBo <encredibo@gmail.com>
2015-06-02 23:30:23 +1000

    Fire now expires properly

--- unity project files/Assets/Scripts/Fire.cs
+++ unity project files/Assets/Scripts/Fire.cs
@@ -8,7 +8,7 @@ public class Fire : Plant {
 	{
 		type = 3;
 		base.Start();
-		lifeSpan = 480;
+		lifeSpan = 50;
 	} 
 	// Update is called once per frame
 	
@@ -21,5 +21,17 @@ public class Fire : Plant {
 			Kill();
 		}
 		base.Update();
+	}
+
+	protected override void Kill(){
+		if (scaleDownCount <= scaleDownFrames)
+		{
+			transform.localScale -= new Vector3(0.005f, 0.005f, 0);
+			scaleDownCount++;
+			return;
+		}
+		spawnControl.Spawn(-type);
+		Destroy(this.gameObject);
+		life = 0;
 	}
 }
